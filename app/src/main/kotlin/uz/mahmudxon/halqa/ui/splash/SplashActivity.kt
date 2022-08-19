8cd645674193000ba92b2a89bb6d199be96151f1
Mahmudxon <mahmudxon98@inbox.ru>
2022-08-19 22:21:49 +0500

    Fix UI

---
+++ app/src/main/kotlin/uz/mahmudxon/halqa/ui/splash/SplashActivity.kt
@@ -0,0 +1,19 @@
+package uz.mahmudxon.halqa.ui.splash
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import dagger.hilt.android.AndroidEntryPoint
+import uz.mahmudxon.halqa.R
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
