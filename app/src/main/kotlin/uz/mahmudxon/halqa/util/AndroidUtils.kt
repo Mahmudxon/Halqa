d6fe5e42dbc087379d46a9b42069da0b7c40c4d9
Mahmudxon <mahmudxon98@inbox.ru>
2022-09-06 20:05:45 +0500

    Long to string as File size

--- app/src/main/kotlin/uz/mahmudxon/halqa/util/AndroidUtils.kt
+++ app/src/main/kotlin/uz/mahmudxon/halqa/util/AndroidUtils.kt
@@ -7,6 +7,25 @@ val Int.dp: Int
     get() =
         (this * Resources.getSystem().displayMetrics.density).toInt()
 
+
+fun Long.toStringAsFileSize(): String {
+    var temp = this.toFloat()
+    if (temp < 1024)
+        return "${temp.toInt()} B"
+
+    temp /= 1024
+
+    if (temp < 1024)
+        return "%.2f KB".format(temp)
+    temp /= 1024
+
+    if (temp < 1024)
+        return "%.2f MB".format(temp)
+    temp /= 1024
+    return "%.2f GB".format(temp)
+}
+
+
 fun getFakeList(): ArrayList<AudioBook> = ArrayList<AudioBook>().also {
 
     it.add(
